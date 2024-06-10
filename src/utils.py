5e7beb50f6b24987bb7d5637f5921872753f2155
sainaidu81 <sairamnaidu99@gmail.com>
2024-06-10 19:28:43 -0400

    Hyper parameter Training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.Exception import CustomException
 
@@ -19,23 +20,30 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
-        for model_name, model in models.items():  # Iterate over both model names and instances
-            model.fit(X_train, y_train)  # Train the model
+        for model_name, model in models.items():
+            para = param[model_name]
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)  # Perform the grid search and fitting together
+
+            # Update model with best parameters found by GridSearchCV
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)  # Explicitly refit model with best parameters
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[model_name] = test_model_score  # Use the model name as the key in the report
+            report[model_name] = test_model_score 
 
-        return report  # Return the full report after all models have been processed
-    
+        return report
+
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e,sys)
 
 
 
